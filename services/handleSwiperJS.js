a4d029b166fb40132c4f10a41e777604c3a4c7e4
Phạm Công Vinh <pcv.fed@gmail.com>
2025-02-26 20:10:17 +0700

    Cập nhật liên kết và code phần Page[Shop] Part 02

---
+++ services/handleSwiperJS.js
@@ -0,0 +1,65 @@
+//TODO: HANDLE SLIDER TOP SALE
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  spaceBetween: 10,
+  loop: true,
+
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+//TODO: HANDLE SLIDER TOP PLAYER
+var swiper = new Swiper(".topSwiper", {
+  slidesPerView: "auto",
+  spaceBetween: 10,
+  loop: true,
+
+  /* The `autoplay` option in the Swiper configuration is used to enable automatic sliding of the
+  slides in the slider. In this case, `autoplay: { delay: 5000 }` specifies that the slider will
+  automatically transition to the next slide every 5000 milliseconds (5 seconds). This means that
+  each slide will be displayed for 5 seconds before moving to the next slide in the sequence. */
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
