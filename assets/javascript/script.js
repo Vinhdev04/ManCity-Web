ce403c387c4fb8e9addd6f6f172cab654b00e682
Phạm Công Vinh <pcv.fed@gmail.com>
2025-02-28 01:05:25 +0700

    Update code full Shop Page_01 - chưa hoàn thiện UI

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -16,3 +16,20 @@ $(document).ready(function () {
     $("#largeModal").modal("hide");
   }, 8000);
 });
+
+//TODO: LOAD CODE[SUB-MENU] FROME FILE "navbar.html" to "index.html"
+//TODO: LOAD CODE[FOOTER] FROME FILE "footer.html" to "index.html"
+//TODO: LOAD CODE[NAVBAR] FROME FILE "navbar.html" to "index.html"
+function loadHTML(elementId, file) {
+  fetch(file)
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById(elementId).innerHTML = data;
+    })
+    .catch((error) => alert("Lỗi trong quá trình tải và đọc dữ liệu! ", error));
+}
+
+// Load header and footer
+window.onload = function () {
+  loadHTML("navbar", "/partials/navbar.html");
+};
--- services/handleSwiperJS.js
+++ services/handleSwiperJS.js
@@ -63,3 +63,32 @@ var swiper = new Swiper(".topSwiper", {
     },
   },
 });
+
+var myswiper = new Swiper(".feedbackSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 5000,
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
